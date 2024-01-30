f9ed36fbea9e5078f8f021e44cee7292b815a1b6
kseniia <k.azovceva@affstar.com>
2024-01-30 11:26:19 +0300

    add fonts, add general styles and set up sass, classnameps npm packages

--- src/components/UI/SearchInput/SearchInput.tsx
+++ src/components/UI/SearchInput/SearchInput.tsx
@@ -1,6 +1,6 @@
 
 
-const Header = () => {
+const SearchInput = () => {
     return (
         <div>
 
@@ -8,4 +8,4 @@ const Header = () => {
     );
 };
 
-export default Header;
+export default SearchInput;
