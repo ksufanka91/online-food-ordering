1dc2335a46907577a68dcbf80098c9da13d37fb5
kseniia <k.azovceva@affstar.com>
2024-01-30 11:36:25 +0300

    remove Categories from Layout component

--- src/components/layout/Layout/Layout.tsx
+++ src/components/layout/Layout/Layout.tsx
@@ -1,16 +1,12 @@
 import {Outlet} from "react-router-dom";
 import Header from "../Header/Header";
-import Categories from "../Categories/Categories";
 import Footer from "../Footer/Footer";
 
 const Layout = () => {
     return (
         <div>
             <Header/>
-            <Categories/>
-
             <Outlet/>
-
             <Footer/>
         </div>
     );
