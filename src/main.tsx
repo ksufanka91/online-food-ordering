93756d7baf126ed283de58c840ac497ab83cd215
kseniia <k.azovceva@affstar.com>
2024-01-30 11:34:58 +0300

    install normalize css

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,8 @@ import {StrictMode} from 'react'
 import {createRoot} from 'react-dom/client'
 import {RouterProvider} from 'react-router-dom';
 import router from '../src/routes/Routes';
-import './index.scss'
+import './index.scss';
+import 'normalize.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
