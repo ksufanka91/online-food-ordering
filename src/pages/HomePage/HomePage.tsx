6ea4a84f7a40382e7b826aac19d4a63de7fca76d
kseniia <k.azovceva@affstar.com>
2024-02-01 17:30:05 +0300

    add Contacts component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
 // import styles from "./HomePage.module.scss";
 import Hero from "../../components/content/Hero/Hero";
+import Contacts from "../../components/content/Contacts/Contacts";
 
 const HomePage = () => {
     return (
         <>
             <Hero/>
+            <Contacts/>
         </>
     );
 };
