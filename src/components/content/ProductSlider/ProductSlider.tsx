fd5825f264f3893c1427b4d1a8ff6b696767e84d
kseniia <k.azovceva@affstar.com>
2024-02-06 16:06:52 +0300

    create store, get data from server

---
+++ src/components/content/ProductSlider/ProductSlider.tsx
@@ -0,0 +1,17 @@
+import {FC} from "react";
+import Product from "@/app/models/Product";
+
+type Props = {
+    title: string;
+    products: Product[]
+}
+
+const ProductSlider: FC<Props> = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default ProductSlider;
--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,32 @@
 import Hero from "@/components/content/Hero/Hero";
 import Contacts from "@/components/content/Contacts/Contacts";
+import Card from "@/components/Card/Card";
+import {useEffect} from "react";
+import {fetchProducts} from "@/app/features/catalog/catalogSlice";
+import {useAppDispatch, useAppSelector} from "@/app/hooks";
 
 const HomePage = () => {
+
+    const dispatch = useAppDispatch();
+
+    const [products, loading] = useAppSelector(state => {
+        return [
+            state.catalog.products,
+            state.catalog.loading,
+        ];
+    });
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
     return (
         <>
             <Hero/>
+            {loading && 'load data'}
+
+            {!loading && products.map(product => <Card key={product.id} product={product}/>)}
+
             <Contacts showAboutUs={true}/>
         </>
     );
