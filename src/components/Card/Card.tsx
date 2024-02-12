71d7e7e5779dd9aef33ac5b03260bc2a13b2de1a
kseniia <k.azovceva@affstar.com>
2024-02-12 10:34:02 +0300

    add Card component

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,6 +1,7 @@
 import {Link} from "react-router-dom";
 import Product from "@/app/models/Product";
 import {FC} from "react";
+import styles from "./Card.module.scss";
 
 type Props = {
     product: Product
@@ -8,9 +9,21 @@ type Props = {
 
 const Card: FC<Props> = ({product}) => {
     return (
-        <div>
-            <Link to={'/'}>{product.name}</Link>
-        </div>
+        <Link to={'/'} className={styles.card}>
+            <img src={product.image} alt="product-image" className={styles.image}/>
+            <div className={styles.cardInfo}>
+                <div className={styles.titleBox}>
+                    <div className={styles.title}>{product.name}</div>
+                    <div className={styles.weight}>{product.weight}</div>
+                </div>
+                <div className={styles.description}>{product.description}</div>
+                <div className={styles.buyInfo}>
+                    <div className={styles.price}>{product.price} &#8381;</div>
+                    <button></button>
+                </div>
+            </div>
+        </Link>
+
     );
 };
 
