8a51586b501d77ba833e43dad365b7677c3a00ed
kseniia <k.azovceva@affstar.com>
2024-02-20 11:23:10 +0300

    add new Routes

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -1,21 +1,24 @@
-import {createBrowserRouter} from "react-router-dom";
+import {createHashRouter} from "react-router-dom";
 import Layout from "../components/layout/Layout/Layout";
 import HomePage from "../pages/HomePage/HomePage";
 import DeliveryPage from "../pages/DeliveryPage/DeliveryPage";
 import PromoPage from "../pages/PromoPage/PromoPage"
 import ProductPage from "../pages/ProductPage/ProductPage";
 import Error404Page from "../pages/Error404Page/Error404Page";
+import CartPage from "@/pages/CartPage/CartPage";
+import CategoryPage from "@/pages/CategoryPage/CategoryPage";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
     {
         path: '/',
         Component: Layout,
         children: [
             {path: '', Component: HomePage},
-            // {path: 'cart', Component: CartPage},
+            {path: 'cart', Component: CartPage},
             {path: 'delivery', Component: DeliveryPage},
             {path: 'promo', Component: PromoPage},
-            {path: 'menu/:id', Component: ProductPage},
+            {path: 'menu/:slug', Component: ProductPage},
+            {path: 'menu/category/:slug', Component: CategoryPage},
             {path: 'notfound', Component: Error404Page}
         ]
     }
