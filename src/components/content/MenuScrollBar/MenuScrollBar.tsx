ca362153316502c4c95464af4d14455833bda87f
kseniia <k.azovceva@affstar.com>
2024-02-20 11:19:35 +0300

    add active class when category is chosen

--- src/components/content/MenuScrollBar/MenuScrollBar.tsx
+++ src/components/content/MenuScrollBar/MenuScrollBar.tsx
@@ -1,18 +1,30 @@
+import {FC} from "react";
 import {Link} from "react-router-dom";
 import {useAppSelector} from "@/app/hooks";
+import classNames from "classnames";
 import styles from "./MenuScrollBar.module.scss";
+import Category from "@/app/models/Category";
 
-const MenuScrollBar = () => {
+type Props = {
+    activeCategory?: Category | null,
+}
+
+const MenuScrollBar: FC<Props> = ({activeCategory}) => {
     const categories = useAppSelector(state => {
         return state.category.categories;
-    })
+    });
 
     return (
         <section className={styles.menuScrollBar}>
             <div className="container">
                 <div className={styles.menu}>
                     {categories.map(category => (
-                        <Link to={'/'} className={styles.menuItem}>{category.name}</Link>
+                        <Link to={`/menu/category/${category.slug}`}
+                              key={category.id}
+                              className={classNames(styles.menuItem, {[styles.active]: category.id === activeCategory?.id})}
+                        >
+                            {category.name}
+                        </Link>
                     ))}
                 </div>
             </div>
