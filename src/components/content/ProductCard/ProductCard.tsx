011b7ae7f4d0ce4e54dd0778dc7ea3b5409236c7
kseniia <k.azovceva@affstar.com>
2024-02-22 11:28:25 +0300

    add ProductCard and ProductCardPage

---
+++ src/components/content/ProductCard/ProductCard.tsx
@@ -0,0 +1,64 @@
+import {FC} from "react";
+import Product from "@/app/models/Product";
+import IconCart from "@/icons/cart.svg?react";
+import {useAppDispatch} from "@/app/hooks";
+import {addToCart} from "@/app/features/cart/cartSlice";
+import Button from "@/components/UI/Button/Button";
+import priceFormat from "@/utils/priceFormat";
+import styles from "./ProductCard.module.scss";
+
+type Props = {
+    product: Product,
+}
+
+const ProductCard: FC<Props> = ({product}) => {
+    const dispatch = useAppDispatch();
+
+    return (
+        <div className={styles.card}>
+            <img src={product.image} alt="product" className={styles.image}/>
+            <div className={styles.cardInfo}>
+                <div className={styles.mainInfo}>
+                    <h3 className={styles.title}>{product.name}</h3>
+                    <p className={styles.description}>{product.description}</p>
+                </div>
+                <div className={styles.details}>
+                    <div className={styles.weight}>Вес: {product.weight}</div>
+                    <div className={styles.buttonBox}>
+                        <Button icon={<IconCart/>} type={'button'} onClick={(e) => {
+                            e.preventDefault();
+                            dispatch(addToCart(product));
+                        }}>В корзину</Button>
+
+                        <div className={styles.price}>{priceFormat(product.price)}</div>
+                    </div>
+                    <div className={styles.items}>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>Белки</div>
+                            <div className={styles.itemDescription}>{product.proteins}</div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>Жиры</div>
+                            <div className={styles.itemDescription}>{product.fats}</div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>Углеводы</div>
+                            <div className={styles.itemDescription}>{product.carbohydrates}</div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>Ккал</div>
+                            <div className={styles.itemDescription}>{product.proteins}</div>
+                        </div>
+                        <div className={styles.item}>
+                            <div className={styles.itemTitle}>Вес</div>
+                            <div className={styles.itemDescription}>{product.weight}</div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default ProductCard;
--- src/pages/ProductPage/ProductPage.tsx
+++ src/pages/ProductPage/ProductPage.tsx
@@ -1,8 +1,50 @@
-const ProductPage = () => {
-    return (
-        <div>
+import {useEffect} from "react";
+import {useParams} from "react-router";
+import {useAppDispatch, useAppSelector} from "@/app/hooks";
+import {fetchProducts} from "@/app/features/catalog/catalogSlice";
+import MenuScrollBar from "@/components/content/MenuScrollBar/MenuScrollBar";
+import Contacts from "@/components/content/Contacts/Contacts";
+import ProductCard from "@/components/content/ProductCard/ProductCard";
+import Product from "@/app/models/Product";
+import styles from "./ProductPage.module.scss";
+import MainTitle from "@/components/UI/MainTitle/MainTitle";
+import ProductSlider from "@/components/content/ProductSlider/ProductSlider";
 
-        </div>
+
+const ProductPage = () => {
+    const {slug} = useParams();
+
+    const dispatch = useAppDispatch();
+
+    useEffect(() => {
+        dispatch(fetchProducts());
+    }, []);
+
+    const products = useAppSelector(state => {
+        return state.catalog.products;
+    });
+
+    let product: Product | null = null;
+    if (products.length > 0) {
+        product = products.find(product => product.slug === slug) || null;
+    }
+
+    return (
+        <section className={styles.productPage}>
+            <MenuScrollBar/>
+
+            <div className="container">
+                {product && <ProductCard product={product}/>}
+
+                <MainTitle title="С этим товаром покупают"/>
+            </div>
+
+            <div className="container-lg">
+                <ProductSlider products={products}/>
+            </div>
+
+            <Contacts showAboutUs={false}/>
+        </section>
     );
 };
 
