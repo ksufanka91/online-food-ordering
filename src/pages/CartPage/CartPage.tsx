6e47e7dcede2a6b6b08fa6c03c001757b8576d80
kseniia <k.azovceva@affstar.com>
2024-03-01 16:24:51 +0300

    implement CartPage component

--- src/pages/CartPage/CartPage.tsx
+++ src/pages/CartPage/CartPage.tsx
@@ -1,10 +1,44 @@
-
+import {useAppDispatch, useAppSelector} from "@/app/hooks";
+import MenuScrollBar from "@/components/content/MenuScrollBar/MenuScrollBar";
+import MainTitle from "@/components/UI/MainTitle/MainTitle";
+import Button from "@/components/UI/Button/Button";
+import CartItem from "@/components/content/ProductInCart/CartItem";
+import priceFormat from "@/utils/priceFormat";
+import styles from "./CartPage.module.scss";
 
 const CartPage = () => {
-    return (
-        <div>
+    const dispatch = useAppDispatch();
 
-        </div>
+    const productsInCart = useAppSelector(state => {
+        return state.cart.products;
+    });
+
+
+    const totalPrice = productsInCart.map(item => item.count * item.price).reduce((acc, curr) => acc + curr, 0);
+
+
+    return (
+        <>
+            <MenuScrollBar/>
+
+            <div className="container">
+                <MainTitle title="Корзина"/>
+                {productsInCart.length === 0 && <div>В корзине пока ничего нет</div>}
+
+                <div className={styles.productsBox}>
+                    <div>
+                        {productsInCart.map(product => <CartItem product={product} key={product.id}/>)}
+                    </div>
+                </div>
+
+                {productsInCart.length > 0 && (
+                    <div className={styles.order}>
+                        <div>Итого: <span className={styles.totalPrice}>{priceFormat(totalPrice)}</span></div>
+                        <Button type={"link"} size={"big"}>Оформить заказ</Button>
+                    </div>
+                )}
+            </div>
+        </>
     );
 };
 
